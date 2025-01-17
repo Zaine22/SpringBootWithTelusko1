940ab63701e79899d4bda1dc43efb350ee760abc
Zaine <kyawkokozin223@gmail.com>
2025-01-17 15:42:11 +0630

    first commit

---
+++ src/main/java/com/zaine/simplesWebApp/SimplesWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.zaine.simplesWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplesWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplesWebAppApplication.class, args);
+	}
+
+}
